6c47d010be965bca1312f0fd500525f8c50dfe88
Niels-Erik <nielseirknandal@gmail.com>
2025-09-24 09:59:17 -0400

    winners

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,21 @@ export default function App() {
   const [gameState, setGameState] = useState(initialGameState);
 
   const handleCellClick = (row: number, col: number) => {
-    if (gameState.board[row][col] !== null || (gameState.xSurrender && gameState.oSurrender)) return
+    if (gameState.board[row][col] !== null || (gameState.pass["x"] && gameState.pass["o"])) return
     setGameState(makeMove(gameState, row, col))
   };
 
   const handlePass = () => {
-    // setGameState(prevGameState => ({...prevGameState, `[prevGameState.currentPlayer]Surrender`: winner}))
-    const winner = calculateWinner(gameState)
-    setGameState(prevGameState => ({...prevGameState, winner: winner}))
+    const newGameState = calculateWinner(gameState)
+    // set the gamestate to the return value of the winnerCalculator
+    setGameState(newGameState)
   }
 
   return (
     <div className="p-4">
       <Board board={gameState.board} onCellClick={handleCellClick} />
       <button onClick={handlePass}>Pass</button>
-      {(gameState.xSurrender && gameState.oSurrender) && <div></div>}
+      {(gameState.pass["x"] && gameState.pass["o"] && gameState.winner) && <div>{gameState.winner} Wins!</div>}
     </div>
   );
 }
