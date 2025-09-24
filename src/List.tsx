6f2551b181e5a23e34781e1081f90e7eabcff3dd
Niels-Erik <nielseirknandal@gmail.com>
2025-09-24 17:08:03 -0400

    routing, state, list and create nav, back

---
+++ src/List.tsx
@@ -0,0 +1,18 @@
+const List = (props: {data, handleOpenGame, handleCreateGame}) => {
+    const games = props.data || []
+    const gameElements = games.map(game => (
+        <li key={game.id} onClick={ () => props.handleOpenGame(game.id) }>id: { game.id }, turn: { game.currentPlayer }, Winner: { game.winner || "undecided" }</li>
+    ))
+    
+    return (
+        <>
+            <ul>
+                { gameElements }
+            </ul>
+            <button onClick={props.handleCreateGame}>Create New Game</button>
+        </>
+  
+    )
+}
+
+export default List
