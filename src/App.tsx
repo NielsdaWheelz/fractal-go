8a97543fa0d4351c534ccc662e9cfa3318ff7b80
Niels-Erik <nielseirknandal@gmail.com>
2025-09-24 00:56:34 -0400

    pass, winner beginnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 import Board from "./Board.tsx";
-import { initialGameState, makeMove } from "./go.ts"
+import { initialGameState, makeMove, calculateWinner } from "./go.ts"
 
 export default function App() {
   const [gameState, setGameState] = useState(initialGameState);
 
   const handleCellClick = (row: number, col: number) => {
-    console.log("clicked", row, col);
-    if (gameState.board[row][col] !== null) return
+    if (gameState.board[row][col] !== null || (gameState.xSurrender && gameState.oSurrender)) return
     setGameState(makeMove(gameState, row, col))
   };
+
+  const handlePass = () => {
+    // setGameState(prevGameState => ({...prevGameState, `[prevGameState.currentPlayer]Surrender`: winner}))
+    const winner = calculateWinner(gameState)
+    setGameState(prevGameState => ({...prevGameState, winner: winner}))
+  }
 
   return (
     <div className="p-4">
       <Board board={gameState.board} onCellClick={handleCellClick} />
+      <button onClick={handlePass}>Pass</button>
+      {(gameState.xSurrender && gameState.oSurrender) && <div></div>}
     </div>
   );
 }
